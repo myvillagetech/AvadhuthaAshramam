e397e1acf56ea4dc984eec25dd1d4128cc28b949
J Prashanth Kumar <83943164+prashanthj32@users.noreply.github.com>
2023-02-09 20:11:09 +0530

    added books files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MONGO_KEYS } from './config/keys.config';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(MONGO_KEYS.url),
     UserModule, 
-    AuthModule
+    AuthModule, BooksModule
   ],
   controllers: [AppController],
   providers: [AppService],
