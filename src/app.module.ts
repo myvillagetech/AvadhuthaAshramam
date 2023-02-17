bf00dd57fe3605db802c1576da4ec4319a9d2092
Hari <hari.katakam@simplifyvms.com>
2023-02-17 21:15:28 +0530

    added static files module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,18 +10,25 @@ import { RoomModule } from './room/room.module';
 import { BooksModule } from './books/books.module';
 import { BikhshaModule } from './bikhsha/bikhsha.module';
 import { BikhshaDetailsModule } from './bikhsha-details/bikhsha-details.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(MONGO_KEYS.url),
-    UserModule, 
-    AuthModule, 
-    RoomModule,
-    BooksModule,
-    ConfigModule.forRoot(), RoomModule, BikhshaModule, BikhshaDetailsModule,
-
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        MongooseModule.forRoot(MONGO_KEYS.url),
+        UserModule,
+        AuthModule,
+        RoomModule,
+        BooksModule,
+        ConfigModule.forRoot(),
+        RoomModule,
+        BikhshaModule,
+        BikhshaDetailsModule,
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'static'),
+        }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 import { ValidationPipe } from '@nestjs/common/pipes';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import "reflect-metadata"
+import 'reflect-metadata';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
 
     app.useGlobalPipes(new ValidationPipe());
 
@@ -23,6 +23,6 @@ async function bootstrap() {
         .build();
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('api', app, document);
-  await app.listen(9000);
+    await app.listen(9000);
 }
 bootstrap();
